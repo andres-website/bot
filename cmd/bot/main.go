2fa7212d33cecc64a3c245964696d86020969fcc
Andres <andres.website@yandex.ru>
2023-01-07 01:27:46 +0700

    Изменил bot/main.go на пример из библиотеки и отрефачил стартово

--- cmd/bot/main.go
+++ cmd/bot/main.go
@@ -1,8 +1,48 @@
 package main
 
+import (
+	"log"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+)
+
+const TOKEN = "YOUR_TOKEN"
+
+func main() {
+	bot, err := tgbotapi.NewBotAPI(TOKEN)
+	if err != nil {
+		log.Panic(err)
+	}
+
+	bot.Debug = true
+
+	log.Printf("Authorized on account %s", bot.Self.UserName)
+
+	u := tgbotapi.UpdateConfig{
+		Offset:  0,
+		Timeout: 60,
+	}
+
+	updates := bot.GetUpdatesChan(u)
+
+	for update := range updates {
+		if update.Message != nil { // If we got a message
+			log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
+
+			msg := tgbotapi.NewMessage(update.Message.Chat.ID, "You wrote: "+update.Message.Text)
+			// msg.ReplyToMessageID = update.Message.MessageID
+
+			bot.Send(msg)
+		}
+	}
+}
+
+/* package main
+
 import "fmt"
 
 func main() {
 
 	fmt.Println("Hello!")
 }
+*/
--- readme.md
+++ readme.md
@@ -5,3 +5,16 @@ go mod init github.com/andres-website/bot
 # Запуск
 go run .\cmd\bot\main.go
 make run
+
+# Устанавливает модули из гитхаба
+go mod tidy
+
+# Windows soft
+GNU make
+https://www.gnu.org/software/make/
+
+Gpg4win
+https://www.gpg4win.org/
+
+// gpg.program
+// https://www.gnupg.org/
